e43058cf50033607e997c67c1e4b5cf734b2b614
diegomardu <diegomartins836@gmail.com>
2023-06-28 22:40:24 -0300

    teste

---
+++ src/test/java/io/github/diegomardu/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.diegomardu.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
